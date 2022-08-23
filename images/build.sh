895914177357a53dbba255002253b90c417d6b0e
Jesper Kristensen <cableman@linuxdev.dk>
2022-08-23 11:18:00 +0200

    Added supervirsor image

--- images/build.sh
+++ images/build.sh
@@ -29,12 +29,16 @@ VERSION=1.1.12
 (cd php8.1-fpm/ubuntu && docker build --pull --no-cache --tag=itkdev/php8.1-fpm .)
 (cd php8.1-fpm/alpine && docker build --pull --no-cache --tag=itkdev/php8.1-fpm:alpine .)
 
-#(cd php8.2-fpm/ubuntu && docker build --pull --no-cache --tag=itkdev/php8.1-fpm .)
+#(cd php8.2-fpm/ubuntu && docker build --pull --no-cache --tag=itkdev/php8.2-fpm .)
 (cd php8.2-fpm/alpine && docker build --pull --no-cache --tag=itkdev/php8.2-fpm:alpine .)
 
 (cd drush6 && docker build --pull --no-cache --tag=itkdev/drush6 .)
 
 (cd mariadb && docker build --pull --no-cache --tag=itkdev/mariadb .)
+
+(cd supervisor && docker build --pull --no-cache --build-arg VERSION=7.4 --tag=itkdev/supervirsor-php7.4:alpine .)
+(cd supervisor && docker build --pull --no-cache --build-arg VERSION=8.0 --tag=itkdev/supervirsor-php8.0:alpine .)
+(cd supervisor && docker build --pull --no-cache --build-arg VERSION=8.1 --tag=itkdev/supervirsor-php8.1:alpine .)
 
 docker image tag itkdev/php5.6-fpm itkdev/php5.6-fpm:${VERSION}
 docker push itkdev/php5.6-fpm
@@ -94,6 +98,13 @@ docker image tag itkdev/php8.2-fpm:alpine itkdev/php8.2-fpm:alpine-${VERSION}
 docker push itkdev/php8.2-fpm:alpine
 # docker push itkdev/php8.2-fpm:${VERSION}
 docker push itkdev/php8.2-fpm:alpine-${VERSION}
+
+docker push itkdev/supervirsor-php7.4:alpine
+docker push itkdev/supervirsor-php7.4:alpine-${VERSION}
+docker push itkdev/supervirsor-php8.0:alpine
+docker push itkdev/supervirsor-php8.0:alpine-${VERSION}
+docker push itkdev/supervirsor-php8.1:alpine
+docker push itkdev/supervirsor-php8.1:alpine-${VERSION}
 
 docker image tag itkdev/drush6 itkdev/drush6:${VERSION}
 docker push itkdev/drush6
