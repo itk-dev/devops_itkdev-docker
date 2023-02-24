2c8f89aeb1ed97d9b3d8915d630b2ab0c92f5ef0
Jesper Kristensen <cableman@linuxdev.dk>
2023-02-24 12:23:10 +0100

    Updated images

--- images/build.sh
+++ images/build.sh
@@ -4,7 +4,7 @@
 set -e
 set -x
 
-VERSION=1.1.23
+VERSION=1.1.25
 
 (cd php5.6-fpm/ubuntu && docker build --pull --no-cache --tag=itkdev/php5.6-fpm .)
 (cd php5.6-fpm/alpine && docker build --pull --no-cache --tag=itkdev/php5.6-fpm:alpine .)
@@ -22,8 +22,9 @@ VERSION=1.1.23
 
 (cd php7.4-fpm/ubuntu && docker build --pull --no-cache --tag=itkdev/php7.4-fpm .)
 (cd php7.4-fpm/alpine && docker build --pull --no-cache --tag=itkdev/php7.4-fpm:alpine .)
-(cd php7.4-fpm/alpine-wkhtmltopdf && docker build --no-cache --tag=itkdev/php7.4-fpm:alpine-wkhtmltopdf .)
-(cd php7.4-fpm/ubuntu-wkhtmltopdf && docker build --no-cache --tag=itkdev/php7.4-fpm:wkhtmltopdf .)
+
+# (cd php7.4-fpm/alpine-wkhtmltopdf && docker build --no-cache --tag=itkdev/php7.4-fpm:alpine-wkhtmltopdf .)
+# (cd php7.4-fpm/ubuntu-wkhtmltopdf && docker build --no-cache --tag=itkdev/php7.4-fpm:wkhtmltopdf .)
 
 (cd php8.0-fpm/ubuntu && docker build --pull --no-cache --tag=itkdev/php8.0-fpm .)
 (cd php8.0-fpm/alpine && docker build --pull --no-cache --tag=itkdev/php8.0-fpm:alpine .)
@@ -44,6 +45,11 @@ VERSION=1.1.23
 (cd supervisor && docker build --no-cache --build-arg VERSION=8.0 --tag=itkdev/supervisor-php8.0:alpine .)
 (cd supervisor && docker build --no-cache --build-arg VERSION=8.1 --tag=itkdev/supervisor-php8.1:alpine .)
 
+
+####
+# Push images
+####
+
 docker image tag itkdev/php5.6-fpm itkdev/php5.6-fpm:${VERSION}
 docker image tag itkdev/php5.6-fpm:alpine itkdev/php5.6-fpm:alpine-${VERSION}
 docker image tag itkdev/php5.6-fpm:alpine itkdev/php5.6-fpm:alpine-wkhtmltopdf-${VERSION}
@@ -53,12 +59,6 @@ docker push itkdev/php5.6-fpm:alpine
 docker push itkdev/php5.6-fpm:alpine-${VERSION}
 docker push itkdev/php5.6-fpm:alpine-wkhtmltopdf
 docker push itkdev/php5.6-fpm:alpine-wkhtmltopdf-${VERSION}
-docker trust sign itkdev/php5.6-fpm:latest
-docker trust sign itkdev/php5.6-fpm:${VERSION}
-docker trust sign itkdev/php5.6-fpm:alpine
-docker trust sign itkdev/php5.6-fpm:alpine-${VERSION}
-docker trust sign itkdev/php5.6-fpm:alpine-wkhtmltopdf
-docker trust sign itkdev/php5.6-fpm:alpine-wkhtmltopdf-${VERSION}
 
 docker image tag itkdev/php7.0-fpm itkdev/php7.0-fpm:${VERSION}
 docker image tag itkdev/php7.0-fpm:alpine itkdev/php7.0-fpm:alpine-${VERSION}
@@ -66,10 +66,7 @@ docker push itkdev/php7.0-fpm:latest
 docker push itkdev/php7.0-fpm:alpine
 docker push itkdev/php7.0-fpm:${VERSION}
 docker push itkdev/php7.0-fpm:alpine-${VERSION}
-docker trust sign itkdev/php7.0-fpm:latest
-docker trust sign itkdev/php7.0-fpm:alpine
-docker trust sign itkdev/php7.0-fpm:${VERSION}
-docker trust sign itkdev/php7.0-fpm:alpine-${VERSION}
+
 
 docker image tag itkdev/php7.2-fpm itkdev/php7.2-fpm:${VERSION}
 docker image tag itkdev/php7.2-fpm:alpine itkdev/php7.2-fpm:alpine-${VERSION}
@@ -77,10 +74,6 @@ docker push itkdev/php7.2-fpm:latest
 docker push itkdev/php7.2-fpm:alpine
 docker push itkdev/php7.2-fpm:${VERSION}
 docker push itkdev/php7.2-fpm:alpine-${VERSION}
-docker trust sign itkdev/php7.2-fpm:latest
-docker trust sign itkdev/php7.2-fpm:alpine
-docker trust sign itkdev/php7.2-fpm:${VERSION}
-docker trust sign itkdev/php7.2-fpm:alpine-${VERSION}
 
 docker image tag itkdev/php7.3-fpm itkdev/php7.3-fpm:${VERSION}
 docker image tag itkdev/php7.3-fpm:alpine itkdev/php7.3-fpm:alpine-${VERSION}
@@ -89,11 +82,6 @@ docker push itkdev/php7.3-fpm:alpine
 docker push itkdev/php7.3-fpm:${VERSION}
 docker push itkdev/php7.3-fpm:alpine-${VERSION}
 docker push itkdev/php7.3-fpm:blackfire
-docker trust sign itkdev/php7.3-fpm:latest
-docker trust sign itkdev/php7.3-fpm:alpine
-docker trust sign itkdev/php7.3-fpm:${VERSION}
-docker trust sign itkdev/php7.3-fpm:alpine-${VERSION}
-docker trust sign itkdev/php7.3-fpm:blackfire
 
 docker image tag itkdev/php7.4-fpm itkdev/php7.4-fpm:${VERSION}
 docker image tag itkdev/php7.4-fpm:alpine itkdev/php7.4-fpm:alpine-${VERSION}
@@ -105,16 +93,8 @@ docker push itkdev/php7.4-fpm:${VERSION}
 docker push itkdev/php7.4-fpm:alpine-${VERSION}
 docker push itkdev/php7.4-fpm:alpine-wkhtmltopdf
 docker push itkdev/php7.4-fpm:alpine-wkhtmltopdf-${VERSION}
-docker push itkdev/php7.4-fpm:wkhtmltopdf
-docker push itkdev/php7.4-fpm:wkhtmltopdf-${VERSION}
-docker trust sign itkdev/php7.4-fpm:latest
-docker trust sign itkdev/php7.4-fpm:alpine
-docker trust sign itkdev/php7.4-fpm:${VERSION}
-docker trust sign itkdev/php7.4-fpm:alpine-${VERSION}
-docker trust sign itkdev/php7.4-fpm:alpine-wkhtmltopdf
-docker trust sign itkdev/php7.4-fpm:alpine-wkhtmltopdf-${VERSION}
-docker trust sign itkdev/php7.4-fpm:wkhtmltopdf
-docker trust sign itkdev/php7.4-fpm:wkhtmltopdf-${VERSION}
+# docker push itkdev/php7.4-fpm:wkhtmltopdf
+# docker push itkdev/php7.4-fpm:wkhtmltopdf-${VERSION}
 
 docker image tag itkdev/php8.0-fpm itkdev/php8.0-fpm:${VERSION}
 docker image tag itkdev/php8.0-fpm:alpine itkdev/php8.0-fpm:alpine-${VERSION}
@@ -122,10 +102,6 @@ docker push itkdev/php8.0-fpm:latest
 docker push itkdev/php8.0-fpm:${VERSION}
 docker push itkdev/php8.0-fpm:alpine
 docker push itkdev/php8.0-fpm:alpine-${VERSION}
-docker trust sign itkdev/php8.0-fpm:latest
-docker trust sign itkdev/php8.0-fpm:${VERSION}
-docker trust sign itkdev/php8.0-fpm:alpine
-docker trust sign itkdev/php8.0-fpm:alpine-${VERSION}
 
 docker image tag itkdev/php8.1-fpm itkdev/php8.1-fpm:${VERSION}
 docker image tag itkdev/php8.1-fpm:alpine itkdev/php8.1-fpm:alpine-${VERSION}
@@ -133,10 +109,6 @@ docker push itkdev/php8.1-fpm:latest
 docker push itkdev/php8.1-fpm:${VERSION}
 docker push itkdev/php8.1-fpm:alpine
 docker push itkdev/php8.1-fpm:alpine-${VERSION}
-docker trust sign itkdev/php8.1-fpm:latest
-docker trust sign itkdev/php8.1-fpm:${VERSION}
-docker trust sign itkdev/php8.1-fpm:alpine
-docker trust sign itkdev/php8.1-fpm:alpine-${VERSION}
 
 docker image tag itkdev/php8.2-fpm itkdev/php8.2-fpm:${VERSION}
 docker image tag itkdev/php8.2-fpm:alpine itkdev/php8.2-fpm:alpine-${VERSION}
@@ -144,44 +116,27 @@ docker push itkdev/php8.2-fpm
 docker push itkdev/php8.2-fpm:${VERSION}
 docker push itkdev/php8.2-fpm:alpine
 docker push itkdev/php8.2-fpm:alpine-${VERSION}
-docker trust sign itkdev/php8.2-fpm:alpine
-docker trust sign itkdev/php8.2-fpm:alpine-${VERSION}
-docker trust sign itkdev/php8.2-fpm:latest
-docker trust sign itkdev/php8.2-fpm:${VERSION}
 
 docker image tag itkdev/supervisor-php7.4:alpine itkdev/supervisor-php7.4:alpine-${VERSION}
 docker push itkdev/supervisor-php7.4:alpine
 docker push itkdev/supervisor-php7.4:alpine-${VERSION}
-docker trust sign itkdev/supervisor-php7.4:alpine
-docker trust sign itkdev/supervisor-php7.4:alpine-${VERSION}
 
 docker image tag itkdev/supervisor-php8.0:alpine itkdev/supervisor-php8.0:alpine-${VERSION}
 docker push itkdev/supervisor-php8.0:alpine
 docker push itkdev/supervisor-php8.0:alpine-${VERSION}
-docker trust sign itkdev/supervisor-php8.0:alpine
-docker trust sign itkdev/supervisor-php8.0:alpine-${VERSION}
 
 docker image tag itkdev/supervisor-php8.1:alpine itkdev/supervisor-php8.1:alpine-${VERSION}
 docker push itkdev/supervisor-php8.1:alpine
 docker push itkdev/supervisor-php8.1:alpine-${VERSION}
-docker trust sign itkdev/supervisor-php8.1:alpine
-docker trust sign itkdev/supervisor-php8.1:alpine-${VERSION}
 
 docker image tag itkdev/drush6 itkdev/drush6:${VERSION}
 docker push itkdev/drush6:latest
 docker push itkdev/drush6:${VERSION}
-docker trust sign itkdev/drush6:latest
-docker trust sign itkdev/drush6:${VERSION}
 
 docker image tag itkdev/mariadb itkdev/mariadb:${VERSION}
 docker push itkdev/mariadb:latest
 docker push itkdev/mariadb:${VERSION}
-docker trust sign itkdev/mariadb:latest
-docker trust sign itkdev/mariadb:${VERSION}
 
 docker image tag itkdev/mailhog itkdev/mailhog:${VERSION}
 docker push itkdev/mailhog:latest
 docker push itkdev/mailhog:${VERSION}
-docker trust sign itkdev/mailhog:latest
-docker trust sign itkdev/mailhog:${VERSION}
-
