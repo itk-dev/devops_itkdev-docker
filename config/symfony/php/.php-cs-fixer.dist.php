aa66060025e0219ccf6103b55d2824a088215b33
Mikkel Ricky <rimi@aarhus.dk>
2025-04-08 12:47:28 +0200

    Added config file headers

--- config/symfony/php/.php-cs-fixer.dist.php
+++ config/symfony/php/.php-cs-fixer.dist.php
@@ -1,4 +1,7 @@
 <?php
+// This file is copied from config/symfony/php/.php-cs-fixer.dist.php in https://github.com/itk-dev/devops_itkdev-docker.
+// Feel free to edit the file, but consider making a pull request if you find a general issue with the file.
+
 // https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/blob/master/doc/config.rst
 
 $finder = new PhpCsFixer\Finder();
